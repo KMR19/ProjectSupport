78520b4e9284446449c931230d58d419811485c9
Black Developa <blackdevelopa@gmail.com>
2018-06-11 21:53:39 +0100

    initialized application entry file and set up server

---
+++ app.js
@@ -0,0 +1,19 @@
+// call in the installed dependencies
+const express = require('express');
+
+// set up express
+const app = express();
+
+// set up port number
+const port = 6020;
+
+// set up route
+app.get('/', (request, respond) => {
+  respond.status(200).json({
+    message: 'Welcome to Project Support',
+  });
+});
+
+app.listen(port, (request, respond) => {
+  console.log('Our server is live. Yay!');
+});
