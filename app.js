4a124c5c29228eab53ee0f7fd4a7574f196a0140
Black Developa <blackdevelopa@gmail.com>
2018-06-27 00:42:35 +0100

    added cause CRUD operations

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 // call in the installed dependencies
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import logger from 'morgan';
+import mainRoutes from './server/routes/main';
 
-// set up express
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+
+// connect to mongoose
+mongoose.connect('mongodb://localhost/projectsupport')
+  .then(() => {
+    console.log('database connected.');
+  })
+  .catch(() => {
+  });
+mongoose.Promise = global.Promise;
 
 // set up port number
 const port = 5035;
 
 // set up route
+app.use('/api/', mainRoutes);
+
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Welcome to Project Support',
   });
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Our server is running on port ${port}`);
+});
