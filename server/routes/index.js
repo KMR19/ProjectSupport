2dcb915d231664dd9deef9188c8f2a6f737cfa26
Black Developa <blackdevelopa@gmail.com>
2018-07-05 02:58:57 +0100

    updated homeRoute

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 
 // home route
 const homeRoute = (app) => {
+  app.get('/', (req, res) => {
+    res.redirect('/api');
+  });
+  
   app.get('/api', (req, res) => {
     res.status(200).json({
       message: 'Welcome to Project Support',
